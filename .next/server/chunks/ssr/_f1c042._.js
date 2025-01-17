cc213b7ef119afb1d196e79261ce6ff36356c00b
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-01-17 23:53:33 +0200

    Add Article List View

--- .next/server/chunks/ssr/_f1c042._.js
+++ .next/server/chunks/ssr/_f1c042._.js
@@ -11,6 +11,7 @@ __turbopack_esm__({
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
 ;
 function Home() {
+    const articles = get;
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("section", {
         className: "mx-auto w-11/12 md:w-1/2 flex flex-col gap-16 mb-20",
         children: [
@@ -20,25 +21,25 @@ function Home() {
                     children: "Welcome to my blog"
                 }, void 0, false, {
                     fileName: "[project]/app/page.tsx",
-                    lineNumber: 7,
+                    lineNumber: 8,
                     columnNumber: 3
                 }, this)
             }, void 0, false, {
                 fileName: "[project]/app/page.tsx",
-                lineNumber: 6,
+                lineNumber: 7,
                 columnNumber: 1
             }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("section", {
                 className: "md:grid md:grid-cols-2 flex flex-col gap-10"
             }, void 0, false, {
                 fileName: "[project]/app/page.tsx",
-                lineNumber: 9,
+                lineNumber: 10,
                 columnNumber: 1
             }, this)
         ]
     }, void 0, true, {
         fileName: "[project]/app/page.tsx",
-        lineNumber: 5,
+        lineNumber: 6,
         columnNumber: 1
     }, this);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image";
+import { getCategorisedArticles } from "@/lib/articles";
+import ArticleItemList from "@/components/ArticleItemList";
 
 export default function Home() {
+
+  const articles = getCategorisedArticles()
+
+  console.log(articles, "logged")
   return (
 <section className="mx-auto w-11/12 md:w-1/2 flex flex-col gap-16 mb-20">
 <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
   <h1>Welcome to my blog</h1>
 </header>
 <section className="md:grid md:grid-cols-2 flex flex-col gap-10">
+
+  {articles !== null && Object.keys(articles).map((article) => <ArticleItemList category={article} articles={articles[article]} key={article}/>) }
 
 </section>
 </section>

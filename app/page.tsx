f90ba3532602246fa395d978ec79be013d246027
Mxolisi Masuku <mxolisi.b.masuku@gmail.com>
2025-01-17 22:02:57 +0200

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<section className="mx-auto w-11/12 md:w-1/2 flex flex-col gap-16 mb-20">
+<header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
+  <h1>Welcome to my blog</h1>
+</header>
+<section className="md:grid md:grid-cols-2 flex flex-col gap-10">
+
+</section>
+</section>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        cormorantGaramond: ["var(--font-cormorant-garamond)", "serif"], 
+        poppins: ["var(--font-poppins)", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
